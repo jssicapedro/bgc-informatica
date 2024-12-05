d379ce47740fe7654fc383b0a2f007cba2076d0b
Jéssica Pedro <jessicapedro165@gmail.com>
2024-12-05 15:25:20 +0000

    update páginas
    update de conteudo e design de termos e condições e de politica de privacidade

--- resources/views/client/termoscondicoes.blade.php
+++ resources/views/client/termoscondicoes.blade.php
@@ -1,23 +1,55 @@
-Política de termos e condições
-1.  Termos
-Ao aceder ao site BGC Informática, concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.
-2. Uso de Licença
-É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no site BGC Informática, apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode: 
-Modificar ou copiar os materiais;  usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não comercial);  tentar descompilar ou fazer engenharia reversa de qualquer software contido no site BGC Informática;  remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou  transferir os materiais para outra pessoa ou ‘espelhe’ os materiais em qualquer outro servidor.
-Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser rescindida por BGC Informática a qualquer momento.
-Ao encerrar a visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados na sua posse, seja em formato eletrónico ou impresso.
-3. Isenção de responsabilidade
-Os materiais no site da BGC Informática são fornecidos ‘como estão’. BGC Informática não oferece garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim específico ou não violação de propriedade intelectual, ou outra violação de direitos.
-Além disso, o BGC Informática não garante ou faz qualquer representação relativa à precisão, aos resultados prováveis ou à confiabilidade do uso dos materiais no seu site, ou de outra forma relacionado a esses materiais, ou em sites vinculados a este site.
-4. Limitações
-Em nenhum caso o BGC Informática ou os seus fornecedores serão responsáveis por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro, ou devido à interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais em BGC Informática, mesmo que BGC Informática ou um representante autorizado da BGC Informática tenha sido notificado oralmente, ou por escrito da possibilidade de tais danos.
-Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos consequentes, ou incidentais, essas limitações podem não se aplicar a você.
-5. Precisão dos materiais
-Os materiais exibidos no site da BGC Informática podem incluir erros técnicos, tipográficos ou fotográficos. BGC Informática não garante que qualquer material no seu site seja preciso, completo ou atual. BGC Informática pode fazer alterações nos materiais contidos no seu site a qualquer momento, sem aviso prévio. No entanto, BGC Informática não se compromete a atualizar os materiais.
-6. Links
-A BGC Informática não analisou todos os sites vinculados ao seu site e não é responsável pelo conteúdo de nenhum site vinculado.
-A inclusão de qualquer link não implica endosso por BGC Informática do site. O uso de qualquer site vinculado é por conta e risco do usuário.
-•Modificações
-O BGC Informática pode rever estes termos de serviço do site a qualquer momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual desses termos de serviço.
-•Lei aplicável
-Estes termos e condições são regidos e interpretados de acordo com as leis do BGC Informática e você submete-se irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou localidade.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Política de termos e condições</title>
+    <link rel="stylesheet" href="{{ asset('css/rma.css') }}">
+</head>
+
+<body>
+
+    <!-- Faixa de navegação -->
+    <div class="navbar">
+        <div class="navbar-left">
+            <img src="{{ asset('img/nav/logotipo.png') }}" alt="Logotipo" class="logo">
+            <h1>Política de termos e condições</h1>
+        </div>
+        <div class="navbar-right">
+            <a href="/" class="btn btn-secondary">Página Principal</a>
+        </div>
+    </div>
+    <main>
+        <h1>Política de termos e condições</h1>
+        <h2>1. Termos</h2>
+        <p>Ao aceder ao site BGC Informática, concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos de autor e marcas comerciais aplicáveis.</p>
+        <h2>2. Uso de Licença</h2>
+        <p>É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no site BGC Informática, apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:</p>
+        <p>Modificar ou copiar os materiais; usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não comercial); tentar descompilar ou fazer engenharia reversa de qualquer software contido no site BGC Informática; remover quaisquer direitos de autor ou outras notações de propriedade dos materiais; ou transferir os materiais para outra pessoa ou ‘espelhe’ os materiais em qualquer outro servidor.</p>
+        <p>Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser rescindida por BGC Informática a qualquer momento.</p>
+        <p>Ao encerrar a visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados na sua posse, seja em formato eletrónico ou impresso.</p>
+        <h2>3. Isenção de responsabilidade</h2>
+        <p>Os materiais no site da BGC Informática são fornecidos ‘como estão’. BGC Informática não oferece garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim específico ou não violação de propriedade intelectual, ou outra violação de direitos.</p>
+        <p>Além disso, o BGC Informática não garante ou faz qualquer representação relativa à precisão, aos resultados prováveis ou à confiabilidade do uso dos materiais no seu site, ou de outra forma relacionado a esses materiais, ou em sites vinculados a este site.</p>
+        <h2>4. Limitações</h2>
+        <p>Em nenhum caso o BGC Informática ou os seus fornecedores serão responsáveis por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro, ou devido à interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais em BGC Informática, mesmo que BGC Informática ou um representante autorizado da BGC Informática tenha sido notificado oralmente, ou por escrito da possibilidade de tais danos.</p>
+        <p>Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos consequentes, ou incidentais, essas limitações podem não se aplicar a você.</p>
+        <h2>5. Precisão dos materiais</h2>
+        <p>Os materiais exibidos no site da BGC Informática podem incluir erros técnicos, tipográficos ou fotográficos. BGC Informática não garante que qualquer material no seu site seja preciso, completo ou atual. BGC Informática pode fazer alterações nos materiais contidos no seu site a qualquer momento, sem aviso prévio. No entanto, BGC Informática não se compromete a atualizar os materiais.</p>
+        <h2>6. Links</h2>
+        <p>A BGC Informática não analisou todos os sites vinculados ao seu site e não é responsável pelo conteúdo de nenhum site vinculado.</p>
+        <p>A inclusão de qualquer link não implica endosso por BGC Informática do site. O uso de qualquer site vinculado é por conta e risco do utilizador.</p>
+        <ul>
+            <li>Modificações</li>
+            <p>O BGC Informática pode rever estes termos de serviço do site a qualquer momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual desses termos de serviço.</p>
+            <li>Lei aplicável</li>
+            <p>Estes termos e condições são regidos e interpretados de acordo com as leis do BGC Informática e você submete-se irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou localidade.</p>
+        </ul>
+    </main>
+    <footer>
+        <p> © Copyright 2024 BGC Informática. </p>
+    </footer>
+</body>
+
+</html>
